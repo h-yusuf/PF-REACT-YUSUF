f52784f5c7d6dc3b808954c11e25a85e453dc941
h-yusuf <hafidhy288@gmail.com>
2025-02-12 00:10:09 +0700

    add : vercell analys

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import { Analytics } from "@vercel/analytics/react"
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -16,5 +16,6 @@ import "./assets/css/main.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
+        <Analytics />
     </React.StrictMode>
 );
